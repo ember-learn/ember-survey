4f8084f740327af0afa0ec1c8bc29b2b32d24b86
Tim <pop@pop-os.localdomain>
2020-11-14 14:07:43 +0000

    wip - move past survey results for issue 2

---
+++ app/routes/results/ember-community-survey-2016.js
@@ -0,0 +1,4 @@
+import Route from '@ember/routing/route';
+
+export default class ResultsEmberCommunitySurvey2016Route extends Route {
+}
---
+++ app/routes/results/ember-community-survey-2017.js
@@ -0,0 +1,4 @@
+import Route from '@ember/routing/route';
+
+export default class ResultsEmberCommunitySurvey2017Route extends Route {
+}
